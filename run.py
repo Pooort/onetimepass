aecd8c256b0a5ab53f353c35856dc6f32e6196ed
Oleg Port <oleg.portnikhin@eteron.ru>
2019-04-17 13:05:35 +0300

    v 1.0

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 import onetimepass as otp
-my_secret = 'c6rn omru gl4k dd2k s45l djzz 7ezt seba'
-print(otp.get_totp(my_secret))
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from config import SECRET, EMAIL, PASSWORD
+from helpers import get_web_driver
+
+driver = get_web_driver(headless=False)
+driver.get('https://gmail.com')
+driver.find_element_by_id('identifierId').send_keys(EMAIL)
+driver.find_element_by_id('identifierNext').click()
+password_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//div[@id="password"]//input'))
+    )
+password_input.send_keys(PASSWORD)
+driver.find_element_by_id('passwordNext').click()
+totpPin_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'totpPin'))
+    )
+totpPin_input.send_keys(otp.get_totp(SECRET))
+driver.find_element_by_id('totpNext').click()
